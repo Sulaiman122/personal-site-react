a711a761597f06429c99004f806527d2ba0b2161
Sulaiman122 <33493055+Sulaiman122@users.noreply.github.com>
2021-08-05 01:19:09 +0300

    Add files via upload

---
+++ src/data/resume/degrees.js
@@ -0,0 +1,16 @@
+const degrees = [
+  {
+    school: 'Qassim University',
+    degree: 'Computer Scirence',
+    link: 'https://www.qu.edu.sa',
+    year: 2016,
+  },
+  {
+    school: 'Qassim Buffalo',
+    degree: 'High diploma of Cybersecurity',
+    link: 'https://www.qu.edu.sa',
+    year: 2012,
+  },
+];
+
+export default degrees;
